d54e33ddae51fe7bd194f11c26136a97995dd08d
Michael Lobel <mglobel@gmail.com>
2016-05-12 20:38:48 -0500

    add additional conditional for curet_user to welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   require "stripe"
 
   def index
-    if current_user.configs and current_user.configs['stripe_token']
+    if current_user and current_user.configs and current_user.configs['stripe_token']
       Stripe.api_key = ENV['STRIPE_SECRET_KEY']
       @stripe_user = Stripe::Customer.retrieve(current_user.configs['stripe_token'])
       @stripe_source = @stripe_user.sources.find{ |s| s.id == @stripe_user.default_source }
